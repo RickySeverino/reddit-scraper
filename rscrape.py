db3f9df1530b58a7405b20abfb12e18bc587c10d
RickySeverino <rickyseverino@protonmail.com>
2019-05-31 15:01:45 -0400

    let user enter argument

--- rscrape.py
+++ rscrape.py
@@ -1,10 +1,17 @@
+#!/usr/bin/env python
 
+import sys
 import urllib.request
 import simplejson as json
 from bs4 import BeautifulSoup
 
-#subreddit url goes here
-url = "https://old.reddit.com/r/netsec/new/"
+if len(sys.argv) == 1:
+    print("\nUsage: rscrape.py subreddit")
+    sys.exit()
+elif len(sys.argv) == 2:
+    subreddit = sys.argv[1]
+    
+url = "https://old.reddit.com/r/" + subreddit + "/new/"
 
 #download the URL and extract the content to the variable html
 request = urllib.request.Request(url, headers={'User-Agent': 'Mozilla/5.0'})
@@ -37,5 +44,7 @@ for link in links:
 
     extracted_records.append(record)
 
-with open('output.json', 'w') as outfile:
+outfile = subreddit + ".json"
+
+with open(outfile, 'w') as outfile:
     json.dump(extracted_records, outfile)
